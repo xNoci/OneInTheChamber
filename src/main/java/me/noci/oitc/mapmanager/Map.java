9daef746b82238da71467c300ee46d1d28b6195c
Noci <nocigit@gmail.com>
2021-03-31 12:21:13 +0200

    Added new Map constructor and worldName field

--- src/main/java/me/noci/oitc/mapmanager/Map.java
+++ src/main/java/me/noci/oitc/mapmanager/Map.java
@@ -3,6 +3,8 @@ package me.noci.oitc.mapmanager;
 import com.google.common.collect.Lists;
 import lombok.Getter;
 import lombok.Setter;
+import me.noci.noclib.utils.LocationUtils;
+import me.noci.oitc.Game;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Location;
 
@@ -10,15 +12,30 @@ import java.util.List;
 
 public class Map {
 
-    @Getter @Setter private String mapName = "Unknown";
+    public static final String DEFAULT_MAP_NAME = "Unknown";
+
+    @Getter @Setter private String mapName = DEFAULT_MAP_NAME;
     @Getter @Setter private String builderName = "Unknown";
+    @Getter @Setter private String worldName = null;
     @Getter @Setter private Location spectatorSpawn = null;
     @Getter private final List<Location> playerSpawns = Lists.newArrayList();
 
-    public Map(String mapName) {
+    protected Map() {
+        this(null);
+    }
+
+    protected Map(String mapName) {
         if (StringUtils.isNotBlank(mapName)) {
             this.mapName = mapName;
         }
+    }
+
+
+    public boolean isValid() {
+        if (mapName.equals(DEFAULT_MAP_NAME)) return false;
+        if (spectatorSpawn == null) return false;
+        if (worldName == null) return false;
+        return playerSpawns.size() >= Game.MIN_PLAYER_SPAWNS;
     }
 
 }
