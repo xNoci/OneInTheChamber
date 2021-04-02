ea2b5403d6f7fa51e6064260b1430f5e732ab981
Noci <nocigit@gmail.com>
2021-04-02 11:20:57 +0200

    Change player values on start

--- src/main/java/me/noci/oitc/state/EndingState.java
+++ src/main/java/me/noci/oitc/state/EndingState.java
@@ -1,13 +1,32 @@
 package me.noci.oitc.state;
 
+import me.noci.noclib.api.NocAPI;
 import me.noci.noclib.api.scoreboard.Scoreboard;
 import me.noci.noclib.api.user.User;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class EndingState extends State {
 
     @Override
     public void start() {
+        NocAPI.getOnlineUsers().forEach(user -> {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    Bukkit.getOnlinePlayers().forEach(other -> other.showPlayer(user.getBase()));
+                    user.clearInventoryAndArmor();
+                    user.getBase().setFoodLevel(20);
+                    user.getBase().setHealth(user.getBase().getMaxHealth());
+                    user.getBase().setGameMode(GameMode.SURVIVAL);
+                    user.removePotionEffects();
+                    user.getBase().spigot().setCollidesWithEntities(true);
+                }
+            }.runTask(plugin);
 
+
+        });
     }
 
     @Override
