68a18b88b1ff50998250234376e7222c8235a7b4
Noci <nocigit@gmail.com>
2021-04-04 13:47:37 +0200

    Added command to set lobby spawn

---
+++ src/main/java/me/noci/oitc/commands/SetLobbySpawnCommand.java
@@ -0,0 +1,53 @@
+package me.noci.oitc.commands;
+
+import me.noci.noclib.command.Command;
+import me.noci.noclib.command.CommandData;
+import me.noci.noclib.utils.LocationUtils;
+import me.noci.oitc.OITC;
+import me.noci.oitc.gameutils.Game;
+import me.noci.oitc.state.StateManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@CommandData(name = "setlobbyspawn")
+public class SetLobbySpawnCommand extends Command {
+
+    private final StateManager stateManager;
+    private final Game game;
+
+    public SetLobbySpawnCommand(JavaPlugin plugin, StateManager stateManager, Game game) {
+        super(plugin);
+        this.stateManager = stateManager;
+        this.game = game;
+    }
+
+    @Override
+    public void onPlayerExecute(Player player, String command, String[] args) {
+        if (!player.hasPermission("lobbyspawn")) {
+            player.sendMessage(OITC.NO_PERMISSION);
+            return;
+        }
+
+        if (!stateManager.isState(StateManager.LOBBY_STATE)) {
+            player.sendMessage(String.format("%s§cDieser Command funktioniert nur während der Lobby-Phase.", OITC.PREFIX));
+            return;
+        }
+
+
+        Location lobbySpawn = player.getLocation().clone();
+        game.setLobbySpawn(lobbySpawn);
+        getPlugin().getConfig().set("lobbySpawn", LocationUtils.locationToStringSilently(lobbySpawn));
+        getPlugin().saveConfig();
+        player.sendMessage(String.format("%s§aDu hast den Lobby-Spawn erfolgreich gesetzt.", OITC.PREFIX));
+    }
+
+    @Override
+    public void onConsoleExecute(ConsoleCommandSender sender, String command, String[] args) {
+        sender.sendMessage("§cThis command is only available for a player.");
+    }
+}
