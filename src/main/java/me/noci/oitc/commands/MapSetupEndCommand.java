a532f00856e4d6a4b33feb73472dad40bbe056c4
Noci <nocigit@gmail.com>
2021-04-05 11:21:41 +0200

    Command setupEnd now checks if map is valid

--- src/main/java/me/noci/oitc/commands/MapSetupEndCommand.java
+++ src/main/java/me/noci/oitc/commands/MapSetupEndCommand.java
@@ -2,9 +2,7 @@ package me.noci.oitc.commands;
 
 import me.noci.noclib.command.Command;
 import me.noci.noclib.command.CommandData;
-import me.noci.oitc.gameutils.Game;
 import me.noci.oitc.OITC;
-import me.noci.oitc.mapmanager.MapConfigPhase;
 import me.noci.oitc.mapmanager.MapManager;
 import me.noci.oitc.state.MapConfigState;
 import me.noci.oitc.state.StateManager;
@@ -37,14 +35,8 @@ public class MapSetupEndCommand extends Command {
         }
 
         MapConfigState state = (MapConfigState) stateManager.getCurrentState();
-        if (!state.isPhase(MapConfigPhase.PLAYER_SPAWNS)) {
+        if (!state.getMap().isValid()) {
             player.sendMessage(String.format("%s§cDu musst das Setup erst beenden.", OITC.PREFIX));
-            return;
-        }
-
-        if (state.getMap().getPlayerSpawns().size() < Game.MIN_PLAYER_SPAWNS) {
-            player.sendMessage(String.format("%s§cDu hast nicht genug Spieler-Spawns platziert.", OITC.PREFIX));
-            player.sendMessage(String.format("%sSpawns: §c%s§8/§c%s", OITC.PREFIX, state.getMap().getPlayerSpawns().size(), Game.MIN_PLAYER_SPAWNS));
             return;
         }
 
