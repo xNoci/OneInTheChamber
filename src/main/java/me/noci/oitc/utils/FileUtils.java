9b38942402f9987667d3f9ed800e658c4fbcbcd9
Noci <nocigit@gmail.com>
2021-04-08 12:03:11 +0200

    Added moveFile and methods are now returning a boolean

--- src/main/java/me/noci/oitc/utils/FileUtils.java
+++ src/main/java/me/noci/oitc/utils/FileUtils.java
@@ -9,14 +9,33 @@ import java.nio.file.StandardCopyOption;
 
 public class FileUtils {
 
-    public static void copyDir(Path src, Path dest) {
+    public static boolean moveFile(Path src, Path dest) {
+        try {
+            java.nio.file.Files.move(src, dest, StandardCopyOption.ATOMIC_MOVE);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyFile(File file, Path src, Path dest) {
+        try {
+            java.nio.file.Files.copy(file.toPath(), dest.resolve(src.relativize(file.toPath())), StandardCopyOption.REPLACE_EXISTING);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean copyDir(Path src, Path dest) {
         for (File file : Files.fileTreeTraverser().preOrderTraversal(src.toFile())) {
-            try {
-                java.nio.file.Files.copy(file.toPath(), dest.resolve(src.relativize(file.toPath())), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!copyFile(file, src, dest)) {
+                return false;
             }
         }
+        return true;
     }
 
 }
