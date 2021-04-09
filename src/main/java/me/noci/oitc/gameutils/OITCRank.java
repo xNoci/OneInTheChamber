5c4a603560dd5227a71ec20d2562a773910ab30d
Noci <nocigit@gmail.com>
2021-04-09 10:17:17 +0200

    Added more ranks to OITCranks

--- src/main/java/me/noci/oitc/gameutils/OITCRank.java
+++ src/main/java/me/noci/oitc/gameutils/OITCRank.java
@@ -6,17 +6,24 @@ import org.bukkit.entity.Player;
 
 public enum OITCRank {
 
-    ADMIN('a', "§4Admin §8| §7", ChatColor.DARK_RED),
-    USER('b', "§9User §8| §7", ChatColor.GRAY),
-    SPECTATOR('c', "§7", ChatColor.GRAY);
-
+    ADMIN('a', "prefix.admin", "§4§lAdmin §8┃ §7", ChatColor.DARK_RED),
+    CHAMP('b', "prefix.champ", "§6§lChamp §8┃ §7", ChatColor.GOLD),
+    VIP('c', "prefix.vip", "§e§lVIP §8┃ §7", ChatColor.YELLOW),
+    PLAYER('d', "§7Spieler §8┃ §7", ChatColor.GRAY),
+    SPECTATOR('e', "§7", ChatColor.GRAY);
 
     private final char priority;
+    private final String permission;
     @Getter private final String prefix;
     @Getter private final ChatColor rankColor;
 
     OITCRank(char priority, String prefix, ChatColor rankColor) {
+        this(priority, null, prefix, rankColor);
+    }
+
+    OITCRank(char priority, String permission, String prefix, ChatColor rankColor) {
         this.priority = priority;
+        this.permission = permission;
         this.prefix = prefix;
         this.rankColor = rankColor;
     }
@@ -27,10 +34,17 @@ public enum OITCRank {
         return name;
     }
 
+    private boolean hasPermission(Player player) {
+        return permission == null || player.hasPermission(permission);
+    }
+
     public static OITCRank getRank(Player player, Game game, boolean allowSpectator) {
-        boolean shouldSpectator = !game.getPlayerSet().contains(player.getUniqueId()) || game.getSpectatorSet().contains(player.getUniqueId());
-        if (shouldSpectator && allowSpectator) return SPECTATOR;
-        return player.isOp() ? ADMIN : USER;
+        boolean isSpectator = !game.getPlayerSet().contains(player.getUniqueId()) || game.getSpectatorSet().contains(player.getUniqueId());
+        if (isSpectator && allowSpectator) return SPECTATOR;
+        for (OITCRank value : values()) {
+            if (value.hasPermission(player)) return value;
+        }
+        return PLAYER;
     }
 
 }
