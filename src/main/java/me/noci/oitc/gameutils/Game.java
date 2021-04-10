2daa7560415ba9fded51b5f5848fdff155937de6
Noci <nocigit@gmail.com>
2021-04-10 09:30:19 +0200

    Changed min player

--- src/main/java/me/noci/oitc/gameutils/Game.java
+++ src/main/java/me/noci/oitc/gameutils/Game.java
@@ -65,7 +65,7 @@ public class Game {
         this.plugin = plugin;
         this.mapManager = mapManager;
         this.maxPlayers = maxPlayers;
-        this.playersNeeded = Math.max((int) (maxPlayers * 0.4), 2);
+        this.playersNeeded = Math.max((int) (maxPlayers * 0.2), 2);
     }
 
     public static void giveItems(Player player) {
