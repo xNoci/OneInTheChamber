de7b1db27561e10fc6dc859c92406068c9057d8c
Noci <nocigit@gmail.com>
2021-04-17 19:05:52 +0200

    Implemented MapSetting

--- src/main/java/me/noci/oitc/mapmanager/Map.java
+++ src/main/java/me/noci/oitc/mapmanager/Map.java
@@ -1,17 +1,17 @@
 package me.noci.oitc.mapmanager;
 
 import com.google.common.collect.Lists;
-import lombok.Getter;
-import lombok.Setter;
 import me.noci.noclib.utils.LocationUtils;
 import me.noci.oitc.gameutils.Game;
-import org.apache.commons.lang.StringUtils;
+import me.noci.oitc.mapmanager.settings.MapData;
+import me.noci.oitc.mapmanager.settings.MapSetting;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,43 +19,48 @@ import java.util.stream.Collectors;
 public class Map {
 
     public static final String DEFAULT_MAP_NAME = "Unknown";
+    public static final String DEFAULT_BUILDER_NAME = DEFAULT_MAP_NAME;
 
-    @Getter @Setter private String mapName = DEFAULT_MAP_NAME;
-    @Getter @Setter private String builderName = "Unknown";
-    @Getter @Setter private String worldName = null;
-    @Getter @Setter private Location spectatorSpawn = null;
-    @Setter private List<String> rawPlayerSpawns = Lists.newArrayList();
+    private final MapSetting setting = new MapSetting();
     private final List<Location> playerSpawns = Lists.newArrayList();
 
     public Map() {
-        this(null);
+        set(MapData.MAP_NAME, DEFAULT_MAP_NAME);
+        set(MapData.MAP_BUILDER, DEFAULT_BUILDER_NAME);
+        set(MapData.PLAYER_SPAWNS, Lists.newArrayList());
     }
 
-    public Map(String mapName) {
-        if (StringUtils.isNotBlank(mapName)) {
-            this.mapName = mapName;
-        }
+    public <T> List<T> getList(MapData data, Class<T> type) {
+        return (List<T>) get(data, List.class);
+    }
+
+    public <T> T get(MapData data, Class<T> type) {
+        return setting.get(data, type);
+    }
+
+    public <T> void set(MapData data, T value) {
+        setting.set(data, value);
     }
 
     public boolean isValid() {
-        if (mapName.equals(DEFAULT_MAP_NAME)) return false;
-        if (spectatorSpawn == null) return false;
-        if (worldName == null) return false;
-        return rawPlayerSpawns.size() >= Game.MIN_PLAYER_SPAWNS;
+        if (get(MapData.MAP_NAME, String.class).equals(DEFAULT_MAP_NAME)) return false;
+        if (get(MapData.SPECTATOR_SPAWN, Location.class) == null) return false;
+        if (get(MapData.WORLD_NAME, String.class) == null) return false;
+        return get(MapData.PLAYER_SPAWNS, ArrayList.class).size() >= Game.MIN_PLAYER_SPAWNS;
     }
 
     public void setupWorld(JavaPlugin plugin) {
         new BukkitRunnable() {
             @Override
             public void run() {
-                Bukkit.createWorld(WorldCreator.name(worldName));
+                Bukkit.createWorld(WorldCreator.name(get(MapData.WORLD_NAME, String.class)));
                 loadSpawns();
             }
         }.runTask(plugin);
     }
 
     private void loadSpawns() {
-        List<Location> locations = rawPlayerSpawns.stream().map(LocationUtils::locationFromStringSilently).collect(Collectors.toList());
+        List<Location> locations = getList(MapData.PLAYER_SPAWNS, String.class).stream().map(LocationUtils::locationFromStringSilently).collect(Collectors.toList());
         playerSpawns.clear();
         playerSpawns.addAll(locations);
     }
@@ -68,12 +73,8 @@ public class Map {
         return playerSpawns.iterator();
     }
 
-    public List<String> getRawPlayerSpawns() {
-        return Lists.newArrayList(this.rawPlayerSpawns);
-    }
-
     public void addRawPlayerSpawn(Location location) {
-        this.rawPlayerSpawns.add(LocationUtils.locationToStringSilently(location));
+        getList(MapData.PLAYER_SPAWNS, String.class).add(LocationUtils.locationToStringSilently(location));
     }
 
 }
